8f534247d9bb796452879b02e15d212c1570d436
sepsol <sepsol.works@gmail.com>
2025-04-26 23:58:10 -0700

    Added a barebones item that's not even in creative tab

--- src/main/java/com/sepsol/tutorialmod/item/ModItems.java
+++ src/main/java/com/sepsol/tutorialmod/item/ModItems.java
@@ -5,10 +5,14 @@ import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
 
 public class ModItems {
     public static final DeferredRegister<Item> ITEMS =
             DeferredRegister.create(ForgeRegistries.ITEMS, TutorialMod.MOD_ID);
+
+    public static final RegistryObject<Item> ALEXANDRITE =
+            ITEMS.register("name", () -> new Item(new Item.Properties()));
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
